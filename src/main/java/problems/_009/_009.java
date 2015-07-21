e6f6bbd8c867f1f5b8942c0adbdf67774363979e
Evan <evan@datarank.com>
2015-07-21 09:30:03 -0500

    Started on problem _009

--- src/main/java/problems/_009/_009.java
+++ src/main/java/problems/_009/_009.java
@@ -1,7 +1,37 @@
 package main.java.problems._009;
 
-/**
- * Created by evan on 7/21/15.
- */
+
+import main.resources.mathFunctions;
+
+import java.io.*;
+
 public class _009 {
+
+    public static void main(String[] args) throws IOException {
+        printNumberOfVowelsInList(args[0]);
+    }
+
+    public static void printNumberOfVowelsInList(String fileName) throws IOException {
+        File file = new File(fileName);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            mathFunctions mathFunction = new mathFunctions();
+
+            while ((line = br.readLine()) != null) {
+                double answer = 0;
+
+                answer = numberOfVowelsInString(lineß);
+
+                System.out.print((int) answer + " ");
+            }
+        }
+    }
+
+    public static int numberOfVowelsInString(String str) {
+
+
+
+        return 0;
+    }
 }
