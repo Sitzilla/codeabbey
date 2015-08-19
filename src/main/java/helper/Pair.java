28e4139aa01b0f35281acac40641bbfd2a1752fa
Evan <evan@datarank.com>
2015-08-19 13:20:17 -0500

    Created generic Pair class

---
+++ src/main/java/helper/Pair.java
@@ -0,0 +1,22 @@
+package main.java.helper;
+
+/**
+ * Created by evan on 8/19/15.
+ */
+public class Pair<F, S> {
+    private final F first;
+    private final S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+}
