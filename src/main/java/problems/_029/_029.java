769f715144d0392cb76489c244e8a980f53e34a8
Evan <evan@datarank.com>
2015-10-10 18:08:12 -0500

    Built out _029 core logic

---
+++ src/main/java/problems/_029/_029.java
@@ -0,0 +1,50 @@
+package main.java.problems._029;
+
+import main.java.helper.MathFunctions;
+import main.java.helper.Pair;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by evan on 10/10/15.
+ */
+public class _029 {
+
+    public static void main(String[] args) throws IOException {
+        indexSort(args[0]);
+    }
+
+    public static void indexSort(String fileName) throws IOException {
+        File file = new File(fileName);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            StringBuffer stringBuffer = new StringBuffer();
+            ArrayList<Integer> list = new ArrayList<Integer>();
+
+            while ((line = br.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+
+            list = MathFunctions.stringToIntegerArraylist(stringBuffer.toString());
+            ArrayList <Pair> pairList = new ArrayList<>();
+
+            // integer array to pair arraylist of value and index
+            for (int i = 0; i < list.size(); i++) {
+                Pair pair = new Pair(list.get(i), i);
+                pairList.add(pair);
+
+            }
+
+
+            for (int i = 0; i < list.size(); i++) {
+                System.out.print(MathFunctions.sortArraylistOfPairs(pairList);
+            }
+        }
+    }
+
+}
